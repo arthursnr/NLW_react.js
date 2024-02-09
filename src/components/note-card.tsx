7cd073cb868ced0aced21d09155f1578e3846eaf
Arthur Ribeiro <arthursnr2021@gmail.com>
2024-02-08 23:17:14 -0300

    Deploy

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,18 +1,59 @@
-export function NoteCard() {
-    return (
-        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-            <span className="text-sm font-medium text-slate-300">
-                há 2 dias
-            </span>
-            <p className="text-sm leading-6 text-slate-400">
-                O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where
-            </p>
-            <p className="text-sm leading-6 text-slate-400">
-                Para configurar o plugin, é preciso instalar como abaixo:
-            </p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo culpa exercitationem, obcaecati repellat corrupti architecto recusandae vero, dolor alias necessitatibus fuga, asperiores quibusdam accusantium quae modi iusto cupiditate laboriosam ad!
+import * as Dialog from '@radix-ui/react-dialog'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
+import { Delete } from './delete'
 
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </button>
+
+interface NoteCardsProps {
+    note: {
+        id: string
+        date: Date
+        content: string
+    }
+    onNoteDeleted: (id: string) => void
+}
+
+export function NoteCard({ note, onNoteDeleted }: NoteCardsProps) {
+
+    function handleDeleteNote() {
+        // Chama a função de exclusão passando o ID da nota
+        onNoteDeleted(note.id);
+    }
+
+    return (
+        <Dialog.Root>
+            <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+                <span className="text-sm font-medium text-slate-300">
+                    {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+                </span>
+                <p className="text-sm leading-6 text-slate-400">
+                    {note.content}
+                </p>
+
+
+                <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+                <Dialog.Overlay className='inset-0 fixed bg-black/50' />
+                <Dialog.Content className='fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none'>
+
+                    <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
+                        <X className='size-5' />
+                    </Dialog.Close>
+                    <div className='flex flex-1 flex-col gap-3 p-5'>
+                        <span className="text-sm font-medium text-slate-300">
+                            {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+                        </span>
+
+                        <p className="text-sm leading-6 text-slate-400">
+                            {note.content}
+                        </p>
+                    </div>
+                    <Delete onDelete={handleDeleteNote} />
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root >
     )
 }
