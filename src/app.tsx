80211256eee14d4d02438b62fa15eaa4572f713b
Arthur Ribeiro <115566700+arthursnr@users.noreply.github.com>
2024-02-08 23:52:10 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useState } from 'react'
-import logo from 'src/assets/logo-nlw-expert.svg'
-import { NewNoteCard } from 'src/components/new-note-card'
-import { NoteCard } from 'src/components/note-card.tsx'
+import logo from 'assets/logo-nlw-expert.svg'
+import { NewNoteCard } from 'components/new-note-card'
+import { NoteCard } from 'components/note-card.tsx'
 
 
 interface Note {
